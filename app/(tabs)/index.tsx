58b39b42f90b11faff87626273f86578be632f6d
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-10 15:38:49 +0900

    home screen finished

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,18 +5,24 @@ import { FlashList } from "@shopify/flash-list";
 import { HStack } from "@/components/ui/hstack";
 import { Pressable } from "@/components/ui/pressable";
 import Cart from "@/components/shop/Cart";
-import { ScrollView } from "react-native";
+import { Dimensions, ScrollView } from "react-native";
 import Ttitle from "@/components/shop/Ttitle";
 import { VStack } from "@/components/ui/vstack";
 import { categories, products } from "@/data/index";
 import { useCallback, useState } from "react";
 import Category from "@/components/shop/Category";
+import Product from "@/components/shop/Product";
+import { Button, ButtonText, ButtonIcon } from "@/components/ui/button";
+import { MoveUpRight } from "lucide-react-native";
+import { Box } from "@/components/ui/box";
 
 const blurhash =
   "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
 export default function HomeScreen() {
   const [select, setSelect] = useState(1);
+  const width = Dimensions.get("screen").width;
+  const numColumns = width < 600 ? 2 : width < 768 ? 3 : 4;
 
   const handleSelect = useCallback((id: number) => {
     setSelect(id);
@@ -60,6 +66,25 @@ export default function HomeScreen() {
             showsHorizontalScrollIndicator={false}
           />
           <Ttitle title="Recomended for You" actionText="See All" />
+          <FlashList
+            data={products}
+            numColumns={numColumns}
+            renderItem={({ item }) => <Product {...item} />}
+            keyExtractor={(item) => item.id.toString()}
+            estimatedItemSize={300}
+            showsVerticalScrollIndicator={false}
+            contentContainerClassName="mt-4"
+            ListFooterComponent={() => (
+              <Box className="h-40">
+                <Button className="mx-auto rounded-lg bg-green-400">
+                  <ButtonText className="font-bold" size="lg">
+                    Explore More
+                  </ButtonText>
+                  <ButtonIcon as={MoveUpRight} className="ml-1 text-white" />
+                </Button>
+              </Box>
+            )}
+          />
         </VStack>
       </ScrollView>
     </SafeAreaView>
---
+++ components/shop/Product.tsx
@@ -0,0 +1,61 @@
+import { Image } from "expo-image";
+
+import { Card } from "@/components/ui/card";
+import { Pressable } from "@/components/ui/pressable";
+import { ProductType } from "@/types";
+import { Icon, FavouriteIcon, StarIcon } from "@/components/ui/icon";
+import { HStack } from "@/components/ui/hstack";
+import { VStack } from "@/components/ui/vstack";
+import { Text } from "@/components/ui/text";
+
+const blurhash =
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
+
+const Product = ({
+  id,
+  brand,
+  title,
+  star,
+  quantity,
+  price,
+  discount,
+  image,
+  users,
+}: ProductType) => {
+  return (
+    <Pressable className="mx-2 mb-4">
+      <Card className="p-0">
+        <Image
+          source={image}
+          style={{ width: "100%", aspectRatio: 3 / 4, borderRadius: 5 }}
+          placeholder={blurhash}
+          contentFit="cover"
+          transition={1000}
+        />
+        <Pressable className="absolute right-2 top-2 rounded-full bg-zinc-300/40">
+          <Icon
+            as={FavouriteIcon}
+            className={`m-3 h-5 w-5 ${users.length > 0 && "fill-red-400"} text-red-400`}
+          />
+        </Pressable>
+        <VStack space="xs" className="mt-2">
+          <HStack space="sm" className="items-center">
+            <Text className="font-semibold text-gray-500">{brand}</Text>
+            <Icon as={StarIcon} size="xs" className="text-orange-500" />
+            <Text size="sm">{star}</Text>
+            <Text size="xs" className="text-gray-500">
+              ({quantity})
+            </Text>
+          </HStack>
+          <Text className="line-clamp-1">{title}</Text>
+          <HStack space="sm" className="items-center">
+            <Text className="font-medium text-green-700">${price.toFixed(2)}</Text>
+            {discount > 0 && <Text size="sm" className="line-through text-gray-500">${discount.toFixed(2)}</Text>}
+          </HStack>
+        </VStack>
+      </Card>
+    </Pressable>
+  );
+};
+
+export default Product;
